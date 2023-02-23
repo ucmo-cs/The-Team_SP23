49b67f517a9d0fb86c8e21553884d6041efd9df5
Ashlea Shaffer <ajs20910@ucmo.edu>
2023-02-23 15:12:18 -0600

    02/23/23 Upload current progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,14 @@
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatCardModule } from '@angular/material/card';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
 @NgModule({
   declarations: [
-    AppComponent,
-
-
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    ReactiveFormsModule
-
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
