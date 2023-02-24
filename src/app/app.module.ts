acca8a1ac944eff8844ec4b5826e881d4fa117fe
Ashlea Shaffer <ajs20910@ucmo.edu>
2023-02-24 14:03:49 -0600

    Updated Base-UI #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
