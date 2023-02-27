b36005c2a23439cb065365bc5ac47f5812bba463
Max McGinn <maxmcginn01@gmail.com>
2023-02-27 11:23:48 -0600

    move images

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { HeaderModule } from './app/header.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+platformBrowserDynamic().bootstrapModule(HeaderModule)
+ .catch(err => console.error(err));
