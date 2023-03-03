eb630b764a67346c1b8446353634350a4ccc2f4a
Max McGinn <maxmcginn01@gmail.com>
2023-03-03 10:39:21 -0600

    Add grid styling to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class Header {
-  
+export class Header{
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { HeaderModule } from './app/header.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-platformBrowserDynamic().bootstrapModule(HeaderModule)
- .catch(err => console.error(err));
