08b5b87656db0de1b6459ccbefafeeb422dd85e3
lw <lilydweiss@gmail.com>
2023-03-22 11:13:59 -0500

    Created View Year dropdown box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Year {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'The-Team_SP23';
+
+  years: Year[] = [
+    {value: '1999', viewValue: '1999'},
+    {value: '2000', viewValue: '2000'},
+    {value: '2001', viewValue: '2001'},
+    {value: '2002', viewValue: '2002'},
+    {value: '2003', viewValue: '2003'},
+    {value: '2004', viewValue: '2004'},
+    {value: '2005', viewValue: '2005'},
+    {value: '2006', viewValue: '2006'},
+    {value: '2007', viewValue: '2007'},
+    {value: '2008', viewValue: '2008'},
+    {value: '2009', viewValue: '2009'},
+    {value: '2010', viewValue: '2010'},
+    {value: '2011', viewValue: '2011'},
+    {value: '2012', viewValue: '2012'},
+    {value: '2013', viewValue: '2013'},
+    {value: '2013', viewValue: '2014'},
+    {value: '2013', viewValue: '2015'},
+    {value: '2013', viewValue: '2016'},
+    {value: '2013', viewValue: '2017'},
+    {value: '2013', viewValue: '2018'},
+    {value: '2013', viewValue: '2019'},
+    {value: '2013', viewValue: '2020'},
+    {value: '2013', viewValue: '2021'},
+    {value: '2013', viewValue: '2022'},
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
