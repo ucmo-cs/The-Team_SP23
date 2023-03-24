2b1ec5d0a463d7db1c56fc079fd6aab13ef8e72a
Nikolos Antonio <nikolos.antonio99@gmail.com>
2023-03-23 21:38:59 -0500

    Adding testing functionality for aws
    Adding testing functionality for aws

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatGridListModule} from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from "@angular/material/table";
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { Header } from './header/header.component';
 import { Tabs } from './tabs/tabs.component';
@@ -14,16 +18,21 @@ import { PDPage } from './pages/PDP.component';
 
 import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
+import { PdtTestComponent } from './pdt-test/pdt-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Header,
     Tabs,
-    PDPage
+    PDPage,
+    PdtTestComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    MatInputModule,
+    MatTableModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatGridListModule,
@@ -31,7 +40,8 @@ import { MatCardModule } from '@angular/material/card';
     MatMenuModule,
     MatTabsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pdt-test/pdt-test.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from '../services/api.service';
+import { MatInputModule } from '@angular/material/input';
+import { PDT } from "../models/pdt";
+
+@Component({
+    selector: 'pdppage-test',
+    templateUrl: './pdt-test.component.html',
+    styleUrls: ['./pdt-test.component.css'],
+  })
+
+  export class PdtTestComponent implements OnInit {
+    id = '';
+    year = '';
+
+    displayedColumns = ['ID', 'Date', 'Time Stamp', 'Name', 'Short Term Goals', 'Medium Term Goals', 'Long Term Goals', 'Development Needs', 'Action Plan',  'Emp Signature', 'Super Signature'];
+    dataSource = new MatTableDataSource([{id: "", createdDate: "", createdTimestamp: "", empName: "", shortTermGoals: "", mediumTermGoals: "", longTermGoals: "", developmentNeeds: "", actionPlan: "", empSignature: "", superSignature: ""}]);
+
+    getPdtIDUrl = "https://j1k4keqbsf.execute-api.us-east-2.amazonaws.com/PDT/id/"
+    delPdtIDUrl = "https://j1k4keqbsf.execute-api.us-east-2.amazonaws.com/PDT/id/"
+    getPdtYearUrl = "https://j1k4keqbsf.execute-api.us-east-2.amazonaws.com/PDT/year/"
+    getPdtListUrl = "https://j1k4keqbsf.execute-api.us-east-2.amazonaws.com/PDT"
+
+    constructor(
+      private http: HttpClient,
+      private api: ApiService,
+    ) {} 
+
+    ngOnInit() {}
+
+    doDelPDTById(id: string) {
+      this.http.delete<any>(this.delPdtIDUrl + '/' + id).subscribe( 
+      res => {this.dataSource = new MatTableDataSource(res)},
+      err => {console.log("Error Occured" + err.message);}
+      );
+    }
+
+    doGetPDTById(id: string) {
+      this.http.get<any>(this.getPdtIDUrl + '/' + id).subscribe( 
+      res => {this.dataSource = new MatTableDataSource(res)},
+      err => {console.log("Error Occured" + err.message);}
+      );
+    }
+
+    doGetPDTByYear(year: string) {
+      this.http.get<any>(this.getPdtYearUrl + '/' + year).subscribe( 
+      res => {this.dataSource = new MatTableDataSource(res)},
+      err => {console.log("Error Occured" + err.message);}
+      );
+    }
+
+    doGetPDTList() {
+      this.http.get<any>(this.getPdtListUrl).subscribe( 
+      res => {this.dataSource = new MatTableDataSource(res)},
+      err => {console.log("Error Occured" + err.message);}
+      );
+    }
+
+  }
