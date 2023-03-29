59f30c87a35f3b2515c9a2ae7d07789154a420e9
lw <lilydweiss@gmail.com>
2023-03-29 10:24:13 -0500

    Created buttons for add, delete, export, and save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SaveComponent } from './save/save.component';
+import { ExportComponent } from './export/export.component';
+import { DeleteComponent } from './delete/delete.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaveComponent,
+    ExportComponent,
+    DeleteComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule
