d4577bf1fa8b5266b8ac07b1456f2ef020940229
alexis <AXK64110@ucmo.edu>
2023-04-03 09:53:42 -0500

    supporttheimprovement component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { AppComponent } from './app.component';
 import { ComponentViewYearComponent } from './component-view-year/component-view-year.component';
+import { ComponentSupporttheimprovementComponent } from './component-supporttheimprovement/component-supporttheimprovement.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentViewYearComponent
+    ComponentViewYearComponent,
+    ComponentSupporttheimprovementComponent
   ],
   imports: [
     
---
+++ src/app/component-supporttheimprovement/component-supporttheimprovement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-supporttheimprovement',
+  templateUrl: './component-supporttheimprovement.component.html',
+  styleUrls: ['./component-supporttheimprovement.component.css']
+})
+export class ComponentSupporttheimprovementComponent {
+
+}
