100b06eabcd3be64d1c517898b682ecd21ccf783
Nikolos Antonio <nikolos.antonio99@gmail.com>
2023-04-11 20:17:10 -0500

    Update pdp.component.ts

--- src/app/pdp/pdp.component.ts
+++ src/app/pdp/pdp.component.ts
@@ -12,19 +12,19 @@ export class PDPComponent {
   private baseUrl = environment.apiUrl;
   private pdtUrl = "https://j1k4keqbsf.execute-api.us-east-2.amazonaws.com/PDT"
 
-  empName = ''
-  completeDate = new Date();
-  checkinDate = new Date();
-  futureCheckin = new Date(this.checkinDate.setMonth(this.checkinDate.getMonth()+6))
-  shortGoals = ''
-  mediumGoals = ''
-  longGoals = ''
-  devNeeds = ''
-  actionPlan = ''
-  empSign = ''
-  empSignDate = new Date();
-  superSign = ''
-  superSignDate = new Date();
+  public empName = ''
+  public completeDate = new Date();
+  public checkinDate = new Date();
+  public futureCheckin = new Date(this.checkinDate.setMonth(this.checkinDate.getMonth()+6))
+  public shortGoals = ''
+  public mediumGoals = ''
+  public longGoals = ''
+  public devNeeds = ''
+  public actionPlan = ''
+  public empSign = ''
+  public empSignDate = new Date();
+  public superSign = ''
+  public superSignDate = new Date();
 
   constructor(
     private http: HttpClient,
@@ -32,29 +32,27 @@ export class PDPComponent {
   ) {} 
 
   ngOnInit() {}
-
+  
+  
 
   doPostJob() {
-    const httpOptions = {
-      params:{
-        empName: this.empName,
-        completed: this.completeDate,
-        futureCheckin: this.futureCheckin,
-        shortTermGoals: this.shortGoals,
-        mediumTermGoals: this.mediumGoals,
-        longTermGoals: this.longGoals,
-        developmentNeeds: this.devNeeds,
-        actionPlan: this.actionPlan,
-        empSignature: this.empSign,
-        empSigDate: this.empSignDate,
-        superSignature: this.superSign,
-        superSignDate: this.superSignDate
-      }
+    const data = {
+      empName: this.empName,
+      shortTermGoals: this.shortGoals,
+      mediumTermGoals: this.mediumGoals,
+      longTermGoals: this.longGoals,
+      developmentNeeds: this.devNeeds,
+      actionPlan: this.actionPlan,
+      empSignature: this.empSign,
+      empSigDate: this.empSignDate,
+      superSignature: this.superSign,
+      superSignDate: this.superSignDate
     }
-    this.http.post<any>(this.pdtUrl, httpOptions).subscribe({
+    
+    this.http.post<any>(this.pdtUrl, data).subscribe({
       next: () => {
         console.log('WORKED');
-        console.log(httpOptions);
+        console.log(data);
       },
       error: (eer) => {
         console.log('ERROR: ' + eer.message);
